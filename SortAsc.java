a59962f40c61fdc67cd09cf3833aa2d94e901335
sanchit05 <sanchitjohari5@gmail.com>
2021-09-17 11:03:51 +0530

    Sorting Ascending and serching index;

--- SortAsc.java
+++ SortAsc.java
@@ -1,8 +1,11 @@
+import java.util.*;
 public class SortAsc {    
-    public static void main(String[] args) {        
+    public static void main(String[] args) {  
+    
+    	Scanner sc = new Scanner(System.in);      
             
         //Initialize array     
-        int [] arr = new int [] {5, 2, 8, 7, 1};     
+        int [] arr = new int [] {5, 5, 2, 8, 7, 1};     
         int temp = 0;    
             
         //Displaying elements of original array    
@@ -28,6 +31,34 @@ public class SortAsc {
         System.out.println("Elements of array sorted in ascending order: ");    
         for (int i = 0; i < arr.length; i++) {     
             System.out.print(arr[i] + " ");    
-        }    
+        }  
+        
+        System.out.println("Enter Element you want to Search");
+        int n = sc.nextInt();  
+        
+        System.out.println("Choose case 1 or 2: ");
+        int choose = sc.nextInt();
+        
+        
+        switch(choose){
+        	case 1:
+        		for (int i = 0; i < arr.length; i++) {     
+            			if(arr[i] == n){
+            				System.out.print(i+", ");
+            				break;
+            			}
+        		}
+        		break;
+        	case 2: 
+        		for (int i = 0; i < arr.length; i++) {     
+            			if(arr[i] == n){
+            				System.out.print(i+", ");
+            			}
+        		}
+        		break;
+        	
+        
+         
+     }   
     }    
 }    
